6e315fae06fb96b51d89d15562b67ae040e7f9da
suly <robinjackson@qq.com>
2020-08-14 16:50:22 +0800

    add pool package, for runtime audio cache

---
+++ go_api/pool/pool.go
@@ -0,0 +1,66 @@
+package pool
+
+import (
+	"crypto/md5"
+	"fmt"
+	"go_api/audio"
+	"go_api/err"
+	"sync"
+)
+
+var lock *sync.RWMutex
+var pool map[string]audio.Audio
+
+func init() {
+	lock = &sync.RWMutex{}
+	pool = make(map[string]audio.Audio)
+}
+
+func Put(audio audio.Audio) (string, error) {
+	info, e := audio.Info()
+	if nil != e {
+		return "", e
+	}
+
+	hash := fmt.Sprintf("%x", md5.Sum([]byte(info.FilePath)))
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	if _, ok := pool[hash]; ok {
+		return "", err.New(err.FileExistsInPool, "file already exists in pool")
+	}
+
+	pool[hash] = audio
+	return hash, nil
+}
+
+func Get(hash string) audio.Audio {
+	lock.RLock()
+	defer lock.RUnlock()
+
+	if audio, ok := pool[hash]; ok {
+		return audio
+	}
+
+	return nil
+}
+
+func Del(hash string) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	delete(pool, hash)
+}
+
+func Pop(hash string) audio.Audio {
+	lock.Lock()
+	defer lock.Unlock()
+
+	if audio, ok := pool[hash]; ok {
+		delete(pool, hash)
+		return audio
+	}
+
+	return nil
+}
