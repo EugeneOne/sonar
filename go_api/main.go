e496641f3925f54b8554b6e904ba3c4cc66a2833
suly <robinjackson@qq.com>
2020-08-14 15:15:42 +0800

    add audio player js api functions

--- go_api/main.go
+++ go_api/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+func main() {}
 
+func Open(filePath string) (string, int) {
+	return "", 0
+}
+
+func Play(fp string) int {
+	return 0
+}
+
+func Pause(fp string) int {
+	return 0
+}
+
+func Stop(fp string) int {
+	return 0
+}
+
+func Seek(fp string, position int) int {
+	return 0
+}
+
+func Position(fp string) int {
+	return 0
+}
+
+func Info(fp string) {
+	// todo: return a struct to describe the audio info?
+}
+
+func SetVolume(fp string, volume int) int {
+	return 0
+}
+
+func Close(fp string) int {
+	return 0
 }
